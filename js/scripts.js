88e35b3a4d6abee09bc4c74f49601a9736ac707b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-29 16:43:15 +0300

    Remove js script which sets ScrollSpy since it does not work properly

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,7 @@
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
+    
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
